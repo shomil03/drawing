2372b55492c6a42a5d0f0def1ee275b59d251192
shomil03 <shomil02020@gmail.com>
2023-10-29 22:47:35 +0530

    Initial Commit

---
+++ drawing/drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawingApp.swift
+//  drawing
+//
+//  Created by Shomil Singh on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
